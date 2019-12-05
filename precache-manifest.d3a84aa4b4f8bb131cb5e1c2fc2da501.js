7d2bdbd1df612466f1eff9b7489c5c0b86d9031d
Andres Espitia <espitiaandres123@gmail.com>
2019-12-05 15:12:03 -0500

    Updates

---
+++ precache-manifest.d3a84aa4b4f8bb131cb5e1c2fc2da501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a85b5a7bc7f8e007254e87ab49fc64",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "e55149d70690a95fbfb9",
+    "url": "/pokedex/static/css/main.89760b40.chunk.css"
+  },
+  {
+    "revision": "0413d329a7e5a09b5b4b",
+    "url": "/pokedex/static/js/2.456e3cda.chunk.js"
+  },
+  {
+    "revision": "e55149d70690a95fbfb9",
+    "url": "/pokedex/static/js/main.cfda03de.chunk.js"
+  },
+  {
+    "revision": "27f16e0463aa7cf68513",
+    "url": "/pokedex/static/js/runtime-main.dc93633d.js"
+  },
+  {
+    "revision": "570fcab842fdd7d199a526f2a1e5f0f6",
+    "url": "/pokedex/static/media/pokemon-logo-png.570fcab8.png"
+  }
+]);
